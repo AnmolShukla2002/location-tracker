cdc8ce362e146a022e31ad1c40f7aed744ae9ca5
Anmol Shukla <anmol.28422@gmail.com>
2024-07-06 17:03:45 +0530

    setting view engine

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+app.set("view engine", "ejs");
+app.set(express.static(path.join(__dirname, "public")));
+
 app.get("/", function (req, res) {
   res.send("hello tracker");
 });
