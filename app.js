4217181d23c28e04ec2b8224ff4a85877aba4c5f
Anmol Shukla <anmol.28422@gmail.com>
2024-07-06 20:45:56 +0530

    disconnected markers

--- app.js
+++ app.js
@@ -11,10 +11,16 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
 io.on("connection", function (socket) {
+  console.log(`New client connected: ${socket.id}`);
+
   socket.on("send-location", function (data) {
+    console.log("Received location:", data);
     io.emit("receive-location", { id: socket.id, ...data });
   });
-  console.log("Connected");
+  socket.on("disconnect", function () {
+    console.log(`Client disconnected: ${socket.id}`);
+    io.emit("user-disconnected", socket.id);
+  });
 });
 
 app.get("/", function (req, res) {
