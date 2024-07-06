83ce4be040acdf6856c2edd3ab93a0caa6307c46
Anmol Shukla <anmol.28422@gmail.com>
2024-07-06 21:28:23 +0530

    polylines for tracking

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
+// const express = require("express");
+// const socketio = require("socket.io");
+// const http = require("http");
+// const path = require("path");
+
+// const app = express();
+// const server = http.createServer(app);
+// const io = socketio(server);
+
+// app.set("view engine", "ejs");
+// app.use(express.static(path.join(__dirname, "public")));
+
+// io.on("connection", function (socket) {
+//   console.log(`New client connected: ${socket.id}`);
+
+//   socket.on("send-location", function (data) {
+//     console.log("Received location:", data);
+//     io.emit("receive-location", { id: socket.id, ...data });
+//   });
+//   socket.on("disconnect", function () {
+//     console.log(`Client disconnected: ${socket.id}`);
+//     io.emit("user-disconnected", socket.id);
+//   });
+// });
+
+// app.get("/", function (req, res) {
+//   res.render("index");
+// });
+
+// server.listen(8000, () => {
+//   console.log("Server is running on port 8000");
+// });
+
 const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
@@ -17,6 +50,7 @@ io.on("connection", function (socket) {
     console.log("Received location:", data);
     io.emit("receive-location", { id: socket.id, ...data });
   });
+
   socket.on("disconnect", function () {
     console.log(`Client disconnected: ${socket.id}`);
     io.emit("user-disconnected", socket.id);
--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,47 @@
+// const socket = io();
+
+// if (navigator.geolocation) {
+//   navigator.geolocation.watchPosition(
+//     (position) => {
+//       const { latitude, longitude } = position.coords;
+//       socket.emit("send-location", { latitude, longitude });
+//     },
+//     (error) => {
+//       console.error(error);
+//     },
+//     {
+//       enableHighAccuracy: true,
+//       timeout: 5000,
+//       maximumAge: 0,
+//     }
+//   );
+// }
+
+// const map = L.map("map").setView([0, 0], 16);
+
+// L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+//   attribution: "Anmol Shukla",
+// }).addTo(map);
+
+// const markers = {};
+
+// socket.on("receive-location", (data) => {
+//   const { id, latitude, longitude } = data;
+//   map.setView([latitude, longitude]);
+//   if (markers[id]) {
+//     markers[id].setLatLng([latitude, longitude]);
+//   } else {
+//     markers[id] = L.marker([latitude, longitude]).addTo(map);
+//   }
+// });
+
+// socket.on("user-disconnected", (id) => {
+//   if (markers[id]) {
+//     map.removeLayer(markers[id]);
+//     delete markers[id];
+//   }
+// });
+
 const socket = io();
 
 if (navigator.geolocation) {
@@ -24,20 +68,35 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 }).addTo(map);
 
 const markers = {};
+const polylines = {};
 
 socket.on("receive-location", (data) => {
   const { id, latitude, longitude } = data;
-  map.setView([latitude, longitude]);
+
   if (markers[id]) {
     markers[id].setLatLng([latitude, longitude]);
   } else {
     markers[id] = L.marker([latitude, longitude]).addTo(map);
   }
+
+  if (polylines[id]) {
+    polylines[id].addLatLng([latitude, longitude]);
+  } else {
+    polylines[id] = L.polyline([[latitude, longitude]], {
+      color: "blue",
+    }).addTo(map);
+  }
+
+  map.setView([latitude, longitude]);
 });
 
 socket.on("user-disconnected", (id) => {
   if (markers[id]) {
     map.removeLayer(markers[id]);
     delete markers[id];
+  }
+  if (polylines[id]) {
+    map.removeLayer(polylines[id]);
+    delete polylines[id];
   }
 });
