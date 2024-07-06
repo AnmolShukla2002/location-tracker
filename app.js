d6fa0758c47aae65c4de3eec63e95c57eec23fae
Anmol Shukla <anmol.28422@gmail.com>
2024-07-06 17:34:14 +0530

    fetch user coordinates based on location

--- app.js
+++ app.js
@@ -1,18 +1,23 @@
 const express = require("express");
 const socketio = require("socket.io");
 const http = require("http");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
 app.set("view engine", "ejs");
-app.set(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get("/", function (req, res) {
-  res.send("hello tracker");
+io.on("connection", function (socket) {
+  console.log("Connected");
 });
 
-app.listen(8000, () => {
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+server.listen(8000, () => {
   console.log("Server is running on port 8000");
 });
